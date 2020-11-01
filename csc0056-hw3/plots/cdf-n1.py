4854b722770b3dc1528045d2cc13b301fe02b7cc
Chao Wang <cw@ntnu.edu.tw>
2020-11-01 17:39:51 +0800

    adding example results

---
+++ csc0056-hw3/plots/cdf-n1.py
@@ -0,0 +1,39 @@
+# This example is derived from the following webpage:
+# https://matplotlib.org/3.1.1/gallery/statistics/histogram_cumulative.html
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+fig, (ax1, ax2) = plt.subplots(2, 1)
+fig.subplots_adjust(hspace=0.4)
+
+#### First, plot the empirical result
+y = np.loadtxt('c:/Users/Chao Wang/code/inter-arrival_time.out10N')
+n_bins = len(y)-1
+n, bins, patches = ax1.hist(y, n_bins, density=True, histtype='step',
+          cumulative=True, label='Empirical result', color='g')
+# the following line is used to remove the last point
+patches[0].set_xy(patches[0].get_xy()[:-1])
+ax1.set_xlim(-0.005, 4.0)
+ax1.set_xlabel('Inter-arrival time (second)')
+ax1.set_ylabel('Probability')
+ax1.set_title('Cumulative Distribution Function (N=1)')
+ax1.legend(loc=4)
+
+#### Then plot the theoretical expontential distribution
+G = np.random.default_rng()
+y = G.exponential(scale=0.55, size=898)
+# Now, plot the cumulative distributioin function (CDF)
+n_bins = len(y)-1
+n, bins, patches = ax2.hist(y, n_bins, density=True, histtype='step',
+          cumulative=True, label='Exponential distribution', color='b')
+# the following line is used to remove the last point
+patches[0].set_xy(patches[0].get_xy()[:-1])
+ax2.set_xlim(-0.005, 4.0)
+ax2.set_xlabel('Inter-arrival time (second)')
+ax2.set_ylabel('Probability')
+#ax2.set_title('Cumulative Distribution Function (N=100)')
+ax2.legend(loc=4)
+
+plt.show()
+#plt.savefig('./cdf.pdf')
---
+++ csc0056-hw3/plots/pdf-n1.py
@@ -0,0 +1,35 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+fig, (ax1, ax2) = plt.subplots(2, 1)
+fig.subplots_adjust(hspace=0.4)
+
+#### First, plot the p.d.f. of our empirical result
+y = np.loadtxt('c:/Users/Chao Wang/code/inter-arrival_time.out1N')
+heights, bins = np.histogram(y, bins=100)
+# do normalization
+heights = heights/float(sum(heights))
+binMids = bins[:-1]+np.diff(bins)/2.
+ax1.plot(binMids, heights, label='Empirical result', color='g')
+ax1.set_xlim(-0.005, 3.8)
+ax1.set_ylim(-0.02, 0.085)
+ax1.set_xlabel('Inter-arrival time (second)')
+ax1.set_ylabel('Probability density')
+ax1.set_title('Probability Density Function (N=1)')
+ax1.legend(loc=1)
+
+#### Now, plot the p.d.f. of exponential distribution
+G = np.random.default_rng()
+y = G.exponential(scale=0.55, size=898)
+heights, bins = np.histogram(y, bins=100)
+# do normalization
+heights = heights/float(sum(heights))
+binMids = bins[:-1]+np.diff(bins)/2.
+ax2.plot(binMids, heights, label='Exponential distribution', color='b')
+ax2.set_xlim(-0.005, 3.8)
+ax2.set_ylim(-0.02, 0.085)
+ax2.set_xlabel('Inter-arrival time (second)')
+ax2.set_ylabel('Probability density')
+ax2.legend(loc=1)
+
+plt.show()
