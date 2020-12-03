763477c980253f126740c262d23e91a3c307b7f8
Chao Wang <cw@ntnu.edu.tw>
2020-12-03 19:19:33 +0800

    rehauling hw4 folder

--- csc0056-hw4/playground/run-random.sh
+++ csc0056-hw4/playground/run-random.sh
@@ -1,47 +1,41 @@
 #!/bin/bash
 
-# In this version we take samples at every arrival to estimate N
-# To make this work, we need to modify ./src/database.c and recompile the code
-SEC=20
-#SEC=50
+# In this version we try to take random samples to estimate N
+# To make random sampling work, we need to modify ./src/database.c and recompile the code
+SAMPLES=5000
 
-#N_PUBS=100
 N_PUBS=100
 
 # The unit of delay is in seconds.
 DELAY=0.4
 
-echo -n "Initializing the system..."
+echo "Initializing the system..."
 # Starting the Mosquitto broker
 stdbuf -oL ../src/mosquitto -c ./hw4.conf > N.log 2> tmp.log &
-for i in $(seq 1 4 1); do
-    sleep 0.5
-    echo -n "."
-done
+sleep 2
 
 # Starting our customized subscriber
 ../client/mosquitto_sub -v -i "sub1" -t "t1" -p 2006 > sub.log &
-echo -n "."
 
 # Starting our customized publishers
 # Note that we've changed the --repeat-delay semantics, and
 # now the delay follows an uniform distribution, with the specified value being the upper bound of the range.
 for i in $(seq 1 1 $N_PUBS); do
     ../client/mosquitto_pub -i "pub$i" -t "t1" -p 2006 -q 0 --embed-timestamp --repeat 50000 --repeat-delay $DELAY &
-    if [ $(( i % 10 )) -eq 0 ]; then
-        echo -n "."
-    fi
     sleep 0.01
 done
 
-echo "Done!"
-echo -n "Collecting samples of N for $SEC seconds..."
+echo "Finished initializing all components."
+echo "Collecting $SAMPLES samples of N..."
 
 # Keep collecting data
-sleep $SEC
 
-echo "Done!"
-echo -n "Start to parse the resulting data..."
+for i in $(seq 1 1 $SAMPLES); do
+    pkill -12 -f src/mosquitto
+    sleep 0.00$(( $RANDOM % 999 + 1))
+done
+
+echo "...done. Start to parse the resulting data..."
 
 # Killing all publishers, the subscriber, and the broker
 # The following manipulation on the file descriptors
@@ -56,15 +50,14 @@ exec 2>&3
 # Now, parse the resulting data.
 # Note that we need to throw away some latency measurements taken during the initialization,
 # for they have relatively long delays due to interference by new client connections.
-# In the following, we throw away the first 300 samples, hoping to discount those interference
+# In the following, we throw away the first 300 lines, hoping to discount those interference
 # incurred by commands in lines 24-27 above.
 awk 'NR>300 {print $0}' sub.log > s1
 awk '{if ($2<0) printf "%.6f\n", (($1-1)+(1000000+$2)/1000000.0); else printf "%.6f\n", ($1+($2/1000000.0))}' s1 > e2e_latency.out
 awk 'NR>300 {print $0}' tmp.log > t1
 awk '{if ($2<0) printf "%.6f\n", (($1-1)+(1000000+$2)/1000000.0); else printf "%.6f\n", ($1+($2/1000000.0))}' t1 > T.log
 
-echo "Done!"
-echo "--------- Experimental Result ---------------"
+echo "----------- result --------------------------"
 # The following 2/$DELAY came from 1/($DELAY/2), since the delay follows an uniform distribution.
 LAMBDA=$(echo "2 / $DELAY * $N_PUBS" | bc)
 T=$(./avg.sh T.log)
@@ -72,7 +65,6 @@ e2e=$(./avg.sh e2e_latency.out)
 echo "  Lambda = $LAMBDA packets/second"
 echo "  T = $T seconds"
 echo "  N (estimated by Little's Theorem) = $(echo "$LAMBDA * $T" | bc) packets"
-awk 'NR>300 {print $0}' N.log > n1
-echo "  N (from our empirical measurement) = $(./avg.sh n1) packets"
+echo "  N (from our empirical measurement) = $(./avg.sh N.log) packets"
 echo "  End-to-end latency = $e2e seconds"
 echo "---------------------------------------------"
